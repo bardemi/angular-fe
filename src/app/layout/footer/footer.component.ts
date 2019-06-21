ea13599d941dc16e8ad28eb4f1fae081de05f4ef
SIMON SCHMIDT <scsi1045@hs-karlsruhe.de>
2019-06-21 14:45:09 +0200

    main und footer layout

---
+++ src/app/layout/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'kunde-footer',
+    template: '',
+})
+export class FooterComponent {
+    constructor() {
+        console.log('FooterComponent.constructor()')
+    }
+}
---
+++ src/app/layout/main/main.module.ts
@@ -0,0 +1,10 @@
+import {MainComponent} from './main.component'
+import {NgModule} from '@angular/core'
+import {RouterModule} from '@angular/router'
+
+@NgModule({
+    declarations: [MainComponent],
+    exports: [MainComponent],
+    imports:[RouterModule],
+})
+export class MainModule {}
