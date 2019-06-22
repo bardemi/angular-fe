04b9cde9e62eb36a815254e349679bc461a10128
Luca Stangohr <lu.stangohr@gmail.com>
2019-06-22 17:41:09 +0200

    e

---
+++ src/app/kunde/create-kunde/create-familienstand.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
+
+@Component({
+    selector: 'kunde-create-familienstand',
+    templateUrl: './create-familienstand.component.html',
+})
+export class CreateFamilienstandComponent implements OnInit {
+    @Input()
+    readonly form!: FormGroup
+
+    readonly familienstand = new FormControl('LEDIG')
+
+    ngOnInit() {
+        console.log('CreateFamilienstandComponent.ngOnInit')
+        // siehe formControlName innerhalb @Component({templateUrl: ...})
+        this.form.addControl('familienstand', this.familienstand)
+    }
+}
