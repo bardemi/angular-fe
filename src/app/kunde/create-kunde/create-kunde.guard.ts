cf44248dea25b5ab3db2f86c265214a10f6c9036
Luca Stangohr <lu.stangohr@gmail.com>
2019-06-22 16:39:57 +0200

    create-kunde.guard.ts

---
+++ src/app/kunde/create-kunde/create-kunde.guard.ts
@@ -0,0 +1,42 @@
+import {
+    ActivatedRouteSnapshot,
+    CanDeactivate,
+    RouterStateSnapshot,
+    UrlTree,
+} from '@angular/router'
+import { CreateKundeComponent } from './create-kunde.component'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { log } from '../../shared'
+
+@Injectable({ providedIn: 'root' })
+export class CreateKundeGuard implements CanDeactivate<CreateKundeComponent> {
+    constructor(){
+        console.log('CreateKundeGuard.constructor()')
+    }
+
+    @log
+    canDeactivate(
+        createKunde: CreateKundeComponent,
+        _: ActivatedRouteSnapshot, // eslint-disable-line @typescript-eslint/no-unused-vars
+        __: RouterStateSnapshot, // eslint-disable-line @typescript-eslint/no-unused-vars
+    ):
+        | Observable<boolean | UrlTree>
+        | Promise<boolean | UrlTree>
+        | boolean
+        | UrlTree {
+        if (createKunde.fertig) {
+            // Seite darf zur gewuenschten URL verlassen werden
+            return true
+        }
+
+        createKunde.showWarning = true
+        createKunde.fertig = true
+        console.warn('Beim Verlassen der Seite gehen Daten verloren.')
+        return false
+    }
+    
+    toString() {
+        return 'CreateKundeGuard'
+    }
+}
